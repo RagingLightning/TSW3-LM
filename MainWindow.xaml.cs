eb6e553a93f1a9001bbb7b627c9242f49f932cd7
RagingLightning <RagingLightningCode@gmail.com>
2022-11-22 14:43:15 +0100

    version 0.9.0: Decompression of TSW3 Liveries

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace TSW3LM
     {
         internal static MainWindow INSTANCE;
 
-        private const string VERSION = "0.3.0";
+        private const string VERSION = "0.9.0";
 
         private Thread InfoCollectorThread = new Thread(GameLiveryInfo.AutoRefresh);
 
